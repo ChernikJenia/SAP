c25dd84f393b088d64a24d60e707552393cfbcbd
Jenia <chernikjenia1999@gmail.com>
2021-09-12 20:31:13 +0300

    lab_2 fixed

---
+++ Lab_2/ErrorMessageLib/framework.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <iostream>
+#include "Winsock2.h"
+
+#define WIN32_LEAN_AND_MEAN        
+
+std::string SetErrorMsgText(int code);
+std::string SetErrorMsgText(std::string msgText, int code);
