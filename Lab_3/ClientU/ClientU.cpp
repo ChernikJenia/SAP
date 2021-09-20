7deea58f9750517d681d7d9c574c40bf1eb482a7
Jenia <chernikjenia1999@gmail.com>
2021-09-20 21:32:08 +0300

    lab_3 was completed

---
+++ Lab_3/ClientU/ClientU.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+#include <string>
+#include "../../Lab_2/ErrorMessageLib/framework.h"
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "../../Lab_2/debug/ErrorMessageLib.lib")
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    system("chcp 1251>nul");
+
+    WSADATA wsaData;
+    SOCKET cS;
+
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup: ", WSAGetLastError());
+
+        if ((cS = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("Socket: ", WSAGetLastError());
+
+        const char* serverAddress = "127.0.0.1";
+
+        if (argc > 1)
+        {
+            serverAddress = argv[1];
+        }
+
+        SOCKADDR_IN to;
+        to.sin_family = AF_INET;
+        to.sin_port = htons(2000);
+        to.sin_addr.s_addr = inet_addr(serverAddress);
+        char ibuf[50]{};
+        int lb = 0,
+            msgCount = 0;
+
+        cout << "Сколько сообщений отправить? ";
+        cin >> msgCount;
+
+        for (int i = 0; i < msgCount; i++)
+        {
+            string msg = "Hello from ClientU " + to_string(i);
+
+            if ((lb = sendto(cS, msg.c_str(), msg.length() + 1, NULL, (sockaddr*)&to, sizeof(to))) == SOCKET_ERROR)
+                throw SetErrorMsgText("sendto: ", WSAGetLastError());
+
+
+            if (lb = recvfrom(cS, ibuf, sizeof(ibuf), NULL, (sockaddr*)&to, &lb) == SOCKET_ERROR)
+                throw SetErrorMsgText("recvfrom:, ", WSAGetLastError());
+
+
+            int k = strlen(ibuf) - 1;
+            string digit = "";
+
+            while (isdigit(ibuf[k]))
+            {
+                digit.insert(0, to_string(ibuf[k--] - 48)); // перевод из ascii
+            }
+
+            msg = "Hello from ClientU " + to_string(atoi(digit.c_str()) + 1);
+
+            if ((lb = sendto(cS, msg.c_str(), msg.length() + 1, NULL, (sockaddr*)&to, sizeof(to))) == SOCKET_ERROR)
+                throw SetErrorMsgText("send: ", WSAGetLastError());
+
+            if ((lb = recvfrom(cS, ibuf, sizeof(ibuf), NULL, (sockaddr*)&to, &lb)) == SOCKET_ERROR)
+                throw SetErrorMsgText("recv: ", WSAGetLastError());
+        }
+
+        if ((lb = sendto(cS, "", 2, NULL, (sockaddr*)&to, sizeof(to))) == SOCKET_ERROR)
+            throw SetErrorMsgText("send: ", WSAGetLastError());
+
+        if (closesocket(cS) == SOCKET_ERROR)
+            throw SetErrorMsgText("CloseSocket: ", WSAGetLastError());
+
+        if (WSACleanup() != 0)
+            throw SetErrorMsgText("Cleanup: ", WSAGetLastError());
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << errorMsgText;
+    }
+
+    return 0;
+}
---
+++ Lab_3/Lab_3/ServerU.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+#include <string>
+#include "../../Lab_2/ErrorMessageLib/framework.h"
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "../../Lab_2/debug/ErrorMessageLib.lib")
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    system("chcp 1251>nul");
+
+    WSADATA wsaData;
+    SOCKET sS;
+
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup: ", WSAGetLastError());
+
+        if ((sS = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("Socket: ", WSAGetLastError());
+
+        SOCKADDR_IN serv;
+        serv.sin_family = AF_INET;
+        serv.sin_port = htons(2000);
+        serv.sin_addr.s_addr = INADDR_ANY;
+
+        if (bind(sS, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR)
+            throw SetErrorMsgText("bind: ", WSAGetLastError());
+
+
+        SOCKADDR_IN from;
+        int lc = sizeof(from);
+        char bfrom[50]{};
+        int lb = 0;
+
+        while (true)
+        {
+            memset(&from, 0, sizeof(from));
+            memset(&bfrom, 0, sizeof(bfrom));
+
+            while(true)
+            {
+                if (lb = recvfrom(sS, bfrom, sizeof(bfrom), NULL, (sockaddr*)&from, &lc) == SOCKET_ERROR)
+                    throw SetErrorMsgText("recvfrom: ", WSAGetLastError());
+
+                if (strlen(bfrom) == 0)
+                {
+                    cout << inet_ntoa(from.sin_addr) << " закончил отправку сообщений...." << endl;
+                    break;
+                }
+
+                cout << "Message from " << inet_ntoa(from.sin_addr) << " : ";
+                cout << bfrom << endl;
+
+                if ((lb = sendto(sS, bfrom, strlen(bfrom) + 1, NULL, (sockaddr*)&from, sizeof(from))) == SOCKET_ERROR)
+                    throw SetErrorMsgText("sendto: ", WSAGetLastError());
+
+            }
+        }
+      
+
+        if (closesocket(sS) == SOCKET_ERROR)
+            throw SetErrorMsgText("CloseSocket: ", WSAGetLastError());
+
+        if (WSACleanup() != 0)
+            throw SetErrorMsgText("Cleanup: ", WSAGetLastError());
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << errorMsgText;
+    }
+
+    return 0;
+
+}
+
